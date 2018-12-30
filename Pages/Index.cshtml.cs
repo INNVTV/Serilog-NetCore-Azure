1fff62a1c7291ecb8f23849dc138aaac77efea7c
Kaz <kaz@innvtv.com>
2018-12-29 21:30:33 -0800

    Added initial post methods

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,14 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
 
 namespace Serilog_NetCore_Azure.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Message = "Default";
+
         public void OnGet()
         {
+            Message = "Get started...";
 
+            Trace.WriteLine("Page gotten.");
+            Debug.WriteLine("Page gotten.");
+            Console.WriteLine("Page gotten.");
+        }
+
+        public void OnPostInfoText()
+        {
+            Message = "Info logged to text";
+
+            Trace.WriteLine("Info logged.");
+            Debug.WriteLine("Info logged.");
+            Console.WriteLine("Info logged.");
+        }
+
+        public void OnPostWarnText()
+        {
+            Message = "Warn logged to text";
+
+            Trace.WriteLine("Info logged.");
+            Debug.WriteLine("Info logged.");
+            Console.WriteLine("Info logged.");
         }
     }
 }
